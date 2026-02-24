f246d41022785c7e396126a5732f91e368d7e60f
Santhosh Sivan <57056830+santhoshsivanva@users.noreply.github.com>
2026-02-24 18:12:22 +0530

    added sg files

---
+++ modules/sg/main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "${terraform.workspace}-alb-sg"
+  description = "Security group for the Applicatio Load Balancer"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-alb-sg"
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "${terraform.workspace}-ec2-sg"
+  description = "Security group for EC2 instances"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
